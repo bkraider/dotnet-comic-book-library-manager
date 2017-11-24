8c00c1cdef14f742da0d5b853d768b484de0ab5d
bhanu korremula <bkraider@gmail.com>
2017-11-24 13:42:57 -0500

    Used Object Oriented Concepts. Move the context instantiation and disposing to the BaseController

--- ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
+++ ComicBookLibraryManagerWebApp/Controllers/ComicBooksController.cs
@@ -15,20 +15,13 @@ namespace ComicBookLibraryManagerWebApp.Controllers
     /// <summary>
     /// Controller for the "Comic Books" section of the website.
     /// </summary>
-    public class ComicBooksController : Controller
+    public class ComicBooksController : BaseController
     {
-
-        private Context _context = null; 
-
-        public ComicBooksController()
-        {
-            _context = new Context();
-        }
 
         public ActionResult Index()
         {
 
-            var comicBooks = _context.ComicBooks
+            var comicBooks = Context.ComicBooks
                     .Include(cb => cb.Series)
                     .OrderBy(cb => cb.Series.Title)
                     .ThenBy(cb => cb.IssueNumber)
@@ -44,7 +37,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var comicBook =   _context.ComicBooks
+            var comicBook =   Context.ComicBooks
                     .Include(cb => cb.Series)
                     .Include(cb => cb.Artists.Select(a => a.Artist))
                     .Include(cb => cb.Artists.Select(a => a.Role))
@@ -67,7 +60,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         {
             var viewModel = new ComicBooksAddViewModel();
 
-            viewModel.Init(_context);
+            viewModel.Init(Context);
 
             return View(viewModel);
         }
@@ -82,15 +75,15 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 var comicBook = viewModel.ComicBook;
                 comicBook.AddArtist(viewModel.ArtistId, viewModel.RoleId);
 
-                _context.ComicBooks.Add(comicBook);
-                _context.SaveChanges();
+                Context.ComicBooks.Add(comicBook);
+                Context.SaveChanges();
 
                 TempData["Message"] = "Your comic book was successfully added!";
 
                 return RedirectToAction("Detail", new { id = comicBook.Id });
             }
 
-            viewModel.Init(_context);
+            viewModel.Init(Context);
 
             return View(viewModel);
         }
@@ -103,7 +96,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var comicBook = _context.ComicBooks
+            var comicBook = Context.ComicBooks
                 .Where(cb => cb.Id == id)
                 .SingleOrDefault();
 
@@ -116,7 +109,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             {
                 ComicBook = comicBook
             };
-            viewModel.Init(_context);
+            viewModel.Init(Context);
 
             return View(viewModel);
         }
@@ -130,15 +123,15 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             {
                 var comicBook = viewModel.ComicBook;
 
-                _context.Entry(comicBook).State = EntityState.Modified;
-                _context.SaveChanges();
+                Context.Entry(comicBook).State = EntityState.Modified;
+                Context.SaveChanges();
 
                 TempData["Message"] = "Your comic book was successfully updated!";
 
                 return RedirectToAction("Detail", new { id = comicBook.Id });
             }
 
-            viewModel.Init(_context);
+            viewModel.Init(Context);
 
             return View(viewModel);
         }
@@ -151,7 +144,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             }
 
             // Include the "Series" navigation property.
-            var comicBook = _context.ComicBooks
+            var comicBook = Context.ComicBooks
                 .Include(cb => cb.Series)
                 .Where(cb => cb.Id == id)
                 .SingleOrDefault();
@@ -168,8 +161,8 @@ namespace ComicBookLibraryManagerWebApp.Controllers
         public ActionResult Delete(int id)
         {
             var comicBook = new ComicBook() { Id = id };
-            _context.Entry(comicBook).State = EntityState.Deleted;
-            _context.SaveChanges();
+            Context.Entry(comicBook).State = EntityState.Deleted;
+            Context.SaveChanges();
 
             TempData["Message"] = "Your comic book was successfully deleted!";
 
@@ -187,7 +180,7 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             if (ModelState.IsValidField("ComicBook.SeriesId") &&
                 ModelState.IsValidField("ComicBook.IssueNumber"))
             {
-                if (_context.ComicBooks
+                if (Context.ComicBooks
                     .Any(cb => cb.Id != comicBook.Id &&
                     cb.SeriesId == comicBook.SeriesId &&
                     cb.IssueNumber == comicBook.IssueNumber))
@@ -198,22 +191,5 @@ namespace ComicBookLibraryManagerWebApp.Controllers
             }
         }
 
-
-        private bool _disposed = false;
-
-        protected override void Dispose(bool disposing)
-        {
-            if (_disposed)
-                return;
-
-            if (disposing)
-            {
-                _context.Dispose();
-            }
-
-            _disposed = true;
-
-            base.Dispose(disposing);
-        }
     }
 }
